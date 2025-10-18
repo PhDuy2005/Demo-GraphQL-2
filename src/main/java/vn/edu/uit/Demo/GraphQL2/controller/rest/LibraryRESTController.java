71c9bd29b0ed2d99aa22b77e6d9a13306fb0fbc1
PhDuy2005 <kduy1706@gmail.com>
2025-10-19 01:53:35 +0700

    format rest api

--- src/main/java/vn/edu/uit/Demo/GraphQL2/controller/rest/LibraryRESTController.java
+++ src/main/java/vn/edu/uit/Demo/GraphQL2/controller/rest/LibraryRESTController.java
@@ -1,12 +1,26 @@
 package vn.edu.uit.Demo.GraphQL2.controller.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.AllArgsConstructor;
+import vn.edu.uit.Demo.GraphQL2.model.Author;
+import vn.edu.uit.Demo.GraphQL2.model.Book;
 import vn.edu.uit.Demo.GraphQL2.service.LibraryService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @AllArgsConstructor
 public class LibraryRESTController {
     private final LibraryService libraryService;
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> getMethodName() {
+        return ResponseEntity.status(HttpStatus.OK).body(new Author());
+    }
+
 }
